6de30c46a5d5d36811829ba35464656f76985448
Atul-project <atulmaurya773@gmail.com>
2020-10-01 23:44:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.broadcastreceiverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendBroadcast(new Intent(this,MyReceiver.class));
+    }
+}
---
+++ app/src/main/java/com/example/broadcastreceiverapp/MyReceiver.java
@@ -0,0 +1,15 @@
+package com.example.broadcastreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show();
+        Log.d("show","hello");
+    }
+}
